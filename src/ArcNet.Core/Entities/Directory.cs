2675a16bccd90b1d5da39f04263cb91ec9bf6be7
matheusknaul <contatomatheush0909@gmail.com>
2026-04-10 23:57:03 -0300

    Create DirectoryNode and FileNode with recursive directory tree implementation

---
+++ src/ArcNet.Core/Entities/Directory.cs
@@ -0,0 +1,56 @@
+namespace ArcNet.Core.Entities;
+
+public class DirectoryNode
+{
+    public string Name { get; private set; }
+    public List<FileNode> Files { get; private set; } = new();
+    public List<DirectoryNode> Directories { get; private set; } = new();
+
+    //Implementar Lazy Load depois
+    //public Directory(DirectoryInfo info, int depth = 0, int maxDepth = 10)
+    public DirectoryNode(DirectoryInfo info)
+    {
+        Name = info.Name;
+        MapAllFiles(info);
+    }
+
+    private void MapAllFiles(DirectoryInfo info)
+    {
+        var files = info.GetFiles();
+
+        foreach (var file in files)
+        {
+            AddFile(new FileNode(file.Name, file.Extension));
+        }
+
+        var directories = info.GetDirectories();
+
+        foreach (var _directory in directories)
+        {
+            var directory = new DirectoryNode(_directory);
+            AddDirectory(directory);
+        }
+    }
+
+    public void AddFile(FileNode file)
+    {
+        Files.Add(file);
+    }
+
+    public void AddDirectory(DirectoryNode directory)
+    {
+        Directories.Add(directory);
+    }
+}
+
+public class FileNode
+{
+    public string Name { get; set; }
+    public string Extension { get; set; }
+
+    public FileNode(string name, string extension)
+    {
+        Name = name;
+        Extension = extension;
+    }
+}
